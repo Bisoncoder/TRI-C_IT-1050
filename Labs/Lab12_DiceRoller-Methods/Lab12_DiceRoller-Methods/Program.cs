ed8ae49adb737785dad7e4912b2814007f314b8c
Ari <nehocira@gmail.com>
2025-06-30 14:13:09 -0400

    Add files via upload

---
+++ Labs/Lab12_DiceRoller-Methods/Lab12_DiceRoller-Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a method that rolls two dice and returns the sum of the two rolls.
+//The method only needs to be called five (5) times.
+
+//Roll counter and output
+for (int r = 0; r < 5; r++)
+{
+    Console.WriteLine("Rolling the dice..."); Console.WriteLine("The results of roll " + (r + 1) + " are: " + DiceTotal());
+    Console.WriteLine();
+}
+
+//Roller
+static int DiceTotal()
+{
+    //Roll two dice and return the sum
+    Random random = new Random();
+    int Dice = random.Next(1, 7) + random.Next(1, 7);
+    return Dice;
+}

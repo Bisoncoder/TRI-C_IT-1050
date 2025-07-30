8b81d3aa35b2aa03c782b0be579015bb078c5e1f
Bisoncoder <nehocira@gmail.com>
2025-07-30 14:25:40 -0400

    Upload Lab 15.

---
+++ Lab15_EmployeeInfo/Lab15_EmployeeInfo/Employee.cs
@@ -0,0 +1,41 @@
+﻿namespace Lab15_EmployeeInfo
+{
+    internal class Employee
+    {
+        //These are the properties of an employee. DO NOT MAKE THESE PROPERTIES PUBLIC!
+        private string name;
+        private string department;
+        private int salary;
+        //Here, you will build out a few methods. These should be public.
+        //Build a constructor here that assigns a name, department, and salary to an employee upon creation
+        public Employee(string name, string department, int salary)
+        {
+            this.name = name;
+            this.department = department;
+            this.salary = salary;
+        }
+
+        //Create a method that gives an employee a raise of 1000
+        public void GiveRaise()
+        {
+            salary += 1000;
+        }
+        //Create a method with the same name (method overloading) that gives an employee a raise of a passed amount
+        public void GiveRaise(int amount)
+        {
+            salary += amount;
+        }
+
+        //Create a method that changes an employee's department
+        public void ChangeDepartment(string newDepartment)
+        {
+            department = newDepartment;
+        }
+        //Create a method that returns an employee's name, department, and salary as a nice looking string
+        public string GetInfo()
+        {
+            return $"Name: {name}, Department: {department}, Salary: {salary:C}"; //C formats the salary as currency
+        }
+        //Do not 'display' the text here, just build the string and return it
+    }
+}
---
+++ Lab15_EmployeeInfo/Lab15_EmployeeInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿//Start with this code for Lab 15. Build out the Employee class first, then complete the Main method.
+using Lab15_EmployeeInfo;
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Create three employee objects
+        //You can completely make up the names, departments, and salaries of each employee
+        Employee emp1 = new Employee("Johnny Boy", "Marketing", 60000);
+        Employee emp2 = new Employee("Sally 'Rivet' Saul", "Engineering", 75000);
+        Employee emp3 = new Employee("Bob", "Construction", 50000); Console.WriteLine();
+
+        //Display the 'info' for each employee
+        Console.WriteLine(emp1.GetInfo());
+        Console.WriteLine(emp2.GetInfo());
+        Console.Write(emp3.GetInfo());
+
+        //Give the first employee a generic raise
+        emp1.GiveRaise();
+
+        //Give change the second employee's department to... something different
+        emp2.ChangeDepartment("Research and Development"); Console.WriteLine();
+
+        //Give the third employee a raise of 5000
+        emp3.GiveRaise(5000); Console.WriteLine();
+
+        //Display the 'info' for each employee again
+        Console.WriteLine(emp1.GetInfo());
+        Console.WriteLine(emp2.GetInfo());
+        Console.WriteLine(emp3.GetInfo());
+    }
+}
